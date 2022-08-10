3f3530bc97a6364dd763974fe2950eb8f36523ca
brightdevs <109676581+brightdevs@users.noreply.github.com>
2022-08-10 14:49:01 -0300

    feature: MVC basic functionality set up
    code coupled into classes for readability

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,28 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-
+import Controller from './interfaces/controller.interface';
 class App {
   public app: express.Application;
   public port: number;
-  constructor(controller: express.RequestHandler, port: number) {
+  constructor(controllers: Controller[], port: number) {
     this.app = express();
     this.port = port;
     this.initializeMiddlewares();
-    this.initializeControllers(controller);
+    this.initializeControllers(controllers);
   }
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
   }
-  private initializeControllers(controller: express.RequestHandler) {
-    this.app.use('/', controller);
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router);
+    });
   }
+
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+      console.log(`⚡️[server]: Server is running at: ${this.port}`);
     });
   }
 }
