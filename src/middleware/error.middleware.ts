270662cca55250766f6810c808d1c5d067258a24
brightdevs <109676581+brightdevs@users.noreply.github.com>
2022-08-11 07:54:47 -0300

    feature: error handling exceptions created

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../exceptions/HttpException';
+
+function errorMiddleware(
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const status = error.status || 500;
+  const message = error.message || 'Internal Error';
+  response.status(status).send({
+    status,
+    message,
+  });
+}
+export default errorMiddleware;
